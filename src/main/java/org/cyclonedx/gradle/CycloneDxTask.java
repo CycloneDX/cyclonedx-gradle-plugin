99c8434bd312e4aa954807694769f249f3bb0cb3
Greg Harris <gharris@angrylogic.net>
2019-05-21 16:56:09 -0400

    Implement minimal BOM generation from Gradle.
    1. Generate component model and generate BOM XML file.
    2. Resolve POM files through Gradle dependency resolution.
    3. Ignore dependencies built as sub-projects.

--- src/main/java/org/cyclonedx/gradle/CycloneDxTask.java
+++ src/main/java/org/cyclonedx/gradle/CycloneDxTask.java
@@ -18,23 +18,31 @@
 package org.cyclonedx.gradle;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.maven.project.MavenProject;
 import org.cyclonedx.BomGenerator;
 import org.cyclonedx.BomParser;
 import org.cyclonedx.model.Component;
+import org.cyclonedx.model.License;
+import org.cyclonedx.util.BomUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.artifacts.ModuleVersionIdentifier;
+import org.gradle.api.artifacts.ResolveException;
 import org.gradle.api.artifacts.ResolvedArtifact;
 import org.gradle.api.artifacts.ResolvedConfiguration;
+import org.gradle.api.artifacts.ResolvedModuleVersion;
 import org.gradle.api.tasks.TaskAction;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -58,50 +66,131 @@ public class CycloneDxTask extends DefaultTask {
         this.buildDir = buildDir;
     }
 
-    /**
-     * This code does not currently work. TESTING ONLY
-     */
     @TaskAction
     public void createBom() {
+
+        Set<String> builtDependencies = getProject()
+            .getRootProject()
+            .getSubprojects()
+            .stream()
+            .map(p -> p.getGroup() + ":" + p.getName() + ":" + p.getVersion())
+            .collect(Collectors.toSet());
+
         Set<Component> components = new LinkedHashSet<>();
-        getLogger().info("PROJECT NAME" + getProject().getName());
         for (Project p : getProject().getAllprojects()) {
-            getLogger().info("ALL-PROJECT NAME" + p.getName());
             for (Configuration configuration : p.getConfigurations()) {
                 if (!shouldSkipConfiguration(configuration)) {
                     ResolvedConfiguration resolvedConfiguration = configuration.getResolvedConfiguration();
                     if (resolvedConfiguration != null) {
                         for (ResolvedArtifact artifact : resolvedConfiguration.getResolvedArtifacts()) {
-                            getLogger().info("RESOLVED ARTIFACt: " + artifact.getName());
-                            getLogger().info("TYPE: " + artifact.getType());
-                            getLogger().info("CLASSIFIER: " + artifact.getClassifier());
-                            getLogger().info("GROUP: " + artifact.getModuleVersion().getId().getGroup());
+                            getLogger().debug("RESOLVED ARTIFACT: " + artifact.getName());
+                            getLogger().debug("TYPE: " + artifact.getType());
+                            getLogger().debug("CLASSIFIER: " + artifact.getClassifier());
+                            getLogger().debug("GROUP: " + artifact.getModuleVersion().getId().getGroup());
+
+                            // Don't include other resources built from this Gradle project.
+                            String dependencyName = getDependencyName(artifact);
+                            if(builtDependencies.stream().anyMatch(c -> c.equals(dependencyName))) {
+                                continue;
+                            }
+
+                            // Convert into a Component and augment with pom metadata if available.
+                            Component component = convertArtifact(artifact);
+                            augmentComponentMetadata(component, dependencyName);
+                            components.add(component);
                         }
                     }
                 }
-                DependencySet ds = configuration.getAllDependencies();
-                for (Dependency dependency : ds) {
-                    getLogger().info(dependency.getGroup() + ":" + dependency.getName() + ":" + dependency.getVersion());
+            }
+        }
+        writeBom(components);
+    }
+
+    public String getDependencyName(ResolvedArtifact artifact) {
+        ModuleVersionIdentifier m = artifact.getModuleVersion().getId();
+        return m.getGroup() + ":" + m.getName() + ":" + m.getVersion();
+    }
+
+    public void augmentComponentMetadata(Component component, String dependencyName) {
+        Dependency pomDep = getProject()
+            .getDependencies()
+            .create(dependencyName + "@pom");
+        Configuration pomCfg = getProject()
+            .getConfigurations()
+            .detachedConfiguration(pomDep);
+        MavenProject project = null;
+
+        try {
+            File pomFile = pomCfg.resolve().stream().findFirst().orElse(null);
+            project = MavenUtils.readPom(pomFile);
+        } catch(IOException err) {
+            getLogger().error("Unable to resolve POM for " + component + ": " + err);
+        } catch(ResolveException err) {
+            getLogger().error("Unable to resolve POM for " + component + ": " + err);
+        }
+
+        if(project != null) {
+            if(project.getOrganization() != null) {
+                component.setPublisher(project.getOrganization().getName());
+            }
+            component.setDescription(project.getDescription());
+            if(project.getLicenses() != null) {
+                final List<License> licenses = new ArrayList<>();
+                for(org.apache.maven.model.License artifactLicense : project.getLicenses()) {
+                    License license = new License();
+                    if(artifactLicense.getName() != null) {
+                        license.setName(artifactLicense.getName());
+                        licenses.add(license);
+                    } else if(artifactLicense.getUrl() != null) {
+                        license.setName(artifactLicense.getUrl());
+                        licenses.add(license);
+                    }
+                }
+                if(licenses.size() > 0) {
+                    component.setLicenses(licenses);
                 }
             }
         }
     }
 
+    public Component convertArtifact(ResolvedArtifact artifact) {
+        final Component component = new Component();
+        component.setGroup(artifact.getModuleVersion().getId().getGroup());
+        component.setName(artifact.getModuleVersion().getId().getName());
+        component.setVersion(artifact.getModuleVersion().getId().getVersion());
+        component.setType("library");
+        try {
+            getLogger().debug(MESSAGE_CALCULATING_HASHES);
+            component.setHashes(BomUtils.calculateHashes(artifact.getFile()));
+        } catch(IOException e) {
+            getLogger().error("Error encountered calculating hashes", e);
+        }
+
+        return component;
+    }
+
+
     private boolean shouldSkipConfiguration(Configuration configuration) {
-        final List<String> skipConfigs = Arrays.asList("apiElements", "implementation", "runtimeElements", "runtimeOnly", "testImplementation", "testRuntimeOnly");
+        final List<String> skipConfigs = Arrays.asList(
+                "apiElements",
+                "implementation",
+                "runtimeElements",
+                "runtimeOnly",
+                "testImplementation",
+                "testRuntimeOnly");
         return skipConfigs.contains(configuration.getName());
     }
 
     /**
      * Ported from Maven plugin.
      */
-    protected void execute(Set<Component> components) throws GradleException{
+    protected void writeBom(Set<Component> components) throws GradleException{
         try {
             getLogger().info(MESSAGE_CREATING_BOM);
             final BomGenerator bomGenerator = new BomGenerator(components);
             bomGenerator.generate();
             final String bomString = bomGenerator.toXmlString();
-            final File bomFile = new File(buildDir, "target/bom.xml");
+            final File bomFile = new File(buildDir, "reports/bom.xml");
             getLogger().info(MESSAGE_WRITING_BOM);
             FileUtils.write(bomFile, bomString, Charset.forName("UTF-8"), false);
             getLogger().info(MESSAGE_VALIDATING_BOM);
