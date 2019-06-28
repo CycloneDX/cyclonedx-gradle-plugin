49acddaed79bfeed9df16a14fef4e99829146d6b
Steve Springett <steve@springett.us>
2019-06-28 10:18:48 -0500

    Correction

--- src/test/createbom.sh
+++ src/test/createbom.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-# NOTEL In order to test un-released SNAPSHOTs on oss.sonatype.org, Gradle v4.1 or higher is required.
+# NOTE: In order to test un-released SNAPSHOTs locally or on oss.sonatype.org,
+# Gradle v4.1 or higher is required.
 
 gradle cyclonedxBom
