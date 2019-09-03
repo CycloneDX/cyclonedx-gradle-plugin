7f8e4c60bfe8a19f4d9ade7621a0cc65df11caf9
Sebastian Schuberth <sschuberth@gmail.com>
2019-09-03 13:26:44 +0200

    CycloneDxPlugin: Set a group for the task to make it visible
    This makes the task show up in "./gradlew tasks". Previously "./gradlew
    tasks --all" was needed.
    
    Fixes #14.

--- src/main/java/org/cyclonedx/gradle/CycloneDxPlugin.java
+++ src/main/java/org/cyclonedx/gradle/CycloneDxPlugin.java
@@ -24,6 +24,7 @@ public class CycloneDxPlugin implements Plugin<Project> {
 
     public void apply(Project project) {
         project.getTasks().create("cyclonedxBom", CycloneDxTask.class, (task) -> {
+            task.setGroup("Reporting");
             task.setBuildDir(project.getBuildDir());
         });
     }
