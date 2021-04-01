9ef01dc83328f153a6872a582db9ef2bf89e6c2a
Steve Springett <steve@springett.us>
2021-04-01 01:33:25 -0500

    removing debug

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ read -p "Really deploy to Maven Central repository (Y/N)? "
 if ( [ "$REPLY" == "Y" ] ) then
 
   mvn clean
-  mvn release:clean release:prepare release:perform -Prelease -X -e | tee release.log
+  mvn release:clean release:prepare release:perform -Prelease -e | tee release.log
 
 else
   echo -e "Exit without deploy"
