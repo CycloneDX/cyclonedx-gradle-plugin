877b47e8113ff81c0b411e47ccd423586e5e8984
Emir Uner <emiruner@gmail.com>
2023-10-11 15:21:00 +0200

    Flatten pom-only dependencies
    Signed-off-by: Emir Uner <emiruner@gmail.com>

--- src/test/groovy/org/cyclonedx/gradle/DependencyResolutionSpec.groovy
+++ src/test/groovy/org/cyclonedx/gradle/DependencyResolutionSpec.groovy
@@ -3,6 +3,7 @@ package org.cyclonedx.gradle
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.cyclonedx.model.Bom
 import org.cyclonedx.model.Component
+import org.cyclonedx.model.Dependency
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import spock.lang.Specification
@@ -41,5 +42,40 @@ class DependencyResolutionSpec extends Specification {
         Component quarkusJackson = bom.getComponents().find(c -> c.name == 'quarkus-jackson')
 
         assert quarkusJackson.getBomRef() != null
+    }
+
+    def "flatten non-jar dependencies"() {
+        given:
+        File testDir = TestUtils.createFromString("""
+            plugins {
+                id 'org.cyclonedx.bom'
+                id 'java'
+            }
+            repositories {
+                mavenCentral()
+            }
+            group = 'com.example'
+            version = '1.0.0'
+
+            dependencies {
+                implementation("org.hibernate:hibernate-core:6.3.1.Final")
+            }""", "rootProject.name = 'hello-world'")
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testDir)
+            .withArguments("cyclonedxBom")
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":cyclonedxBom").outcome == TaskOutcome.SUCCESS
+        File jsonBom = new File(testDir, "build/reports/bom.json")
+        Bom bom = new ObjectMapper().readValue(jsonBom, Bom.class)
+        Dependency root = bom.getDependencies().find(dependency -> dependency.getRef().contains("example"))
+
+        assert bom.getDependencies().find(dependency -> dependency.getRef().contains("pkg:maven/jakarta.inject/jakarta.inject-api@2.0.1?type=jar"))
+        assert root.getDependencies().size() == 1
+        assert root.getDependencies().get(0).getRef() == "pkg:maven/org.hibernate.orm/hibernate-core@6.3.1.Final?type=jar"
     }
 }
