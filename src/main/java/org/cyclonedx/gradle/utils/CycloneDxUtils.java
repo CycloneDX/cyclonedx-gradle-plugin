89d8a65f9c83f7b2bf15afa16152538f3617e99a
skhokhlov <me@skhlv.nyc>
2024-06-06 11:36:49 +0100

    upgrade cyclonedx-core-java to 9.0.2
    Signed-off-by: skhokhlov <me@skhlv.nyc>

--- src/main/java/org/cyclonedx/gradle/utils/CycloneDxUtils.java
+++ src/main/java/org/cyclonedx/gradle/utils/CycloneDxUtils.java
@@ -1,23 +1,24 @@
 package org.cyclonedx.gradle.utils;
 
-import org.cyclonedx.CycloneDxSchema;
+import org.cyclonedx.Version;
 
 public class CycloneDxUtils {
 
-    public static final CycloneDxSchema.Version DEFAULT_SCHEMA_VERSION = CycloneDxSchema.Version.VERSION_15;
+    public static final Version DEFAULT_SCHEMA_VERSION = Version.VERSION_16;
 
     /**
      * Resolves the CycloneDX schema the mojo has been requested to use.
      * @return the CycloneDX schema to use
      */
-    public static CycloneDxSchema.Version schemaVersion(String version) {
+    public static Version schemaVersion(String version) {
         switch (version) {
-            case "1.0": return CycloneDxSchema.Version.VERSION_10;
-            case "1.1": return CycloneDxSchema.Version.VERSION_11;
-            case "1.2": return CycloneDxSchema.Version.VERSION_12;
-            case "1.3": return CycloneDxSchema.Version.VERSION_13;
-            case "1.4": return CycloneDxSchema.Version.VERSION_14;
-            case "1.5": return CycloneDxSchema.Version.VERSION_15;
+            case "1.0": return Version.VERSION_10;
+            case "1.1": return Version.VERSION_11;
+            case "1.2": return Version.VERSION_12;
+            case "1.3": return Version.VERSION_13;
+            case "1.4": return Version.VERSION_14;
+            case "1.5": return Version.VERSION_15;
+            case "1.6": return Version.VERSION_16;
             default: return DEFAULT_SCHEMA_VERSION;
         }
     }
