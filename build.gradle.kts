90acc65eb86267d872b632d3deef3d84fd13f4eb
skhokhlov <me@skhlv.nyc>
2024-06-21 09:57:07 +0100

    bump cyclonedx-core-java to 9.0.3
    Signed-off-by: skhokhlov <me@skhlv.nyc>

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.cyclonedx:cyclonedx-core-java:9.0.2") {
+    implementation("org.cyclonedx:cyclonedx-core-java:9.0.3") {
         exclude(group = "org.apache.logging.log4j", module ="log4j-slf4j-impl")
     }
 
