f223e7d914373c3a228846a0b6fe70cba188abf2
skhokhlov <me@skhlv.nyc>
2024-07-24 20:53:22 +0100

    build: prepare version 1.9.0
    Signed-off-by: skhokhlov <me@skhlv.nyc>

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val organization = "CycloneDX"
 group = "org.cyclonedx"
-version = "1.8.2"
+version = "1.9.0"
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
