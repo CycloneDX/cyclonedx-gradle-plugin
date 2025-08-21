a141db1ccfe820beb47188df286c5566da86d10a
skhokhlov <me@skhlv.nyc>
2025-08-21 17:00:58 +0100

    test: run tests with all supported gradle versions
    Signed-off-by: skhokhlov <me@skhlv.nyc>

---
+++ src/test/groovy/org/cyclonedx/gradle/GradleVersionsSpec.groovy
@@ -0,0 +1,48 @@
+package org.cyclonedx.gradle
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Assumptions
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll("java version: #javaVersion, gradle version: #gradleVersion task name: #taskName")
+class GradleVersionsSpec extends Specification {
+
+    def "should support gradle versions"() {
+        given:
+        File testDir = TestUtils.createFromString("""
+            plugins {
+                id 'org.cyclonedx.bom'
+                id 'java'
+            }
+            repositories {
+                mavenCentral()
+            }
+            group = 'com.example'
+            version = '1.0.0'
+            dependencies {
+                implementation group: 'com.fasterxml.jackson.datatype', name: 'jackson-datatype-jsr310', version:'2.8.11'
+                implementation group: 'org.springframework.boot', name: 'spring-boot-starter-web', version:'1.5.18.RELEASE'
+            }""", "rootProject.name = 'hello-world'")
+
+
+        when:
+        Assumptions.assumeTrue(javaVersion >= 17)
+        def gradleVersionMajor = Integer.parseInt(gradleVersion.split("\\.")[0])
+        Assumptions.assumeFalse(javaVersion < 17 && gradleVersionMajor >= 9)
+        def result = GradleRunner.create()
+        .withGradleVersion(gradleVersion)
+            .withProjectDir(testDir)
+            .withArguments(TestUtils.arguments(taskName))
+            .withPluginClasspath()
+            .build()
+        then:
+        result.task(":" + taskName).outcome == TaskOutcome.SUCCESS
+
+        where:
+        taskName = 'cyclonedxBom'
+        javaVersion = TestUtils.javaVersion
+        gradleVersion << ['9.0.0', '8.14', '8.13', '8.12', '8.11', '8.10', '8.9', '8.8', '8.7']
+    }
+}
